1c8e75547f15f658f8501847d6829a0f5a1ff6b4
muhwezi deo <aggrey256@gmail.com>
2018-12-04 16:08:31 +0300

    setup tests

--- fast_food_api/users/tests.py
+++ fast_food_api/users/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from . import serializers
 # Create your tests here.
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.user_data={
+            "username":"dee",
+            "password":"Adeo256.",
+            "email":"dee@gmail.com"
+        }
+
+
+
+    def test_create_user(self):
+        pass
+        
