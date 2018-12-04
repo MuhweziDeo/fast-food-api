5161a5fcb2a20fd69ffea9886acd51b8c766d3e3
muhwezi deo <aggrey256@gmail.com>
2018-12-04 17:14:20 +0300

    add test for signup

--- fast_food_api/users/tests.py
+++ fast_food_api/users/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 from django.contrib.auth.models import User
 from . import serializers
+from django.urls import reverse
+import json
 # Create your tests here.
 
 class ModelTestCase(TestCase):
@@ -10,9 +12,24 @@ class ModelTestCase(TestCase):
             "password":"Adeo256.",
             "email":"dee@gmail.com"
         }
+        self.test_client=Client()
 
 
 
     def test_create_user(self):
         pass
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.test_client=Client()
+        self.user_data={
+            "username":"dee",
+            "password":"Adeo256.",
+            "email":"dee@gmail.com"
+        }
+    def test_signup(self):
+        resp=self.test_client.post(reverse("signup"),content_type='application/json',data=json.dumps(self.user_data))
+        self.assertEqual(resp.status_code,201)
+        self.assertIn("dee",str(resp.data))
+
         
