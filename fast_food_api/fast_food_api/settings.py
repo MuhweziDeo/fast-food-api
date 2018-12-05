732909bb462732e1808007a8c88fce9a62b5aa9a
muhwezi deo <aggrey256@gmail.com>
2018-12-05 16:57:38 +0300

    add authentication classeses for restframework

--- fast_food_api/fast_food_api/settings.py
+++ fast_food_api/fast_food_api/settings.py
@@ -42,6 +42,15 @@ INSTALLED_APPS = [
     'rest_framework.authtoken'
 ]
 
+REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': (
+'rest_framework.authentication.TokenAuthentication',
+'rest_framework.authentication.SessionAuthentication',
+)
+}
+
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
