874dc1c821d526b67841d0424aba675f1c2a4aae
muhwezi deo <aggrey256@gmail.com>
2018-12-05 16:59:55 +0300

    add authentication and permission classes

--- fast_food_api/users/views.py
+++ fast_food_api/users/views.py
@@ -4,4 +4,6 @@ from . import serializers
 # Create your views here.
 
 class UserView(generics.CreateAPIView):
+    authentication_classes=()
+    permission_classes=()
     serializer_class=serializers.UserSerializer
