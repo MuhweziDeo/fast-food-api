cf60f9bf34e317308ead2a06587e8863d8bb4b58
muhwezi deo <aggrey256@gmail.com>
2018-12-05 17:36:08 +0300

    add view and url for login

--- fast_food_api/users/views.py
+++ fast_food_api/users/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from rest_framework import generics
 from . import serializers
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 # Create your views here.
 
 class UserView(generics.CreateAPIView):
     authentication_classes=()
     permission_classes=()
     serializer_class=serializers.UserSerializer
+
+class LoginView(APIView):
+    permission_classes = ()
+    def post(self, request,):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"})
