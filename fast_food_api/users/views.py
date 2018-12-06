cef42a0d67297030b6930bf9401e3d36e60cd675
muhwezi deo <aggrey256@gmail.com>
2018-12-06 08:50:15 +0300

    override save method (bug) enable saving password hashes

--- fast_food_api/users/views.py
+++ fast_food_api/users/views.py
@@ -5,3 +5,9 @@ from . import serializers
 
 class UserView(generics.CreateAPIView):
     serializer_class=serializers.UserSerializer
+
+    def perform_create(self,serializer):
+        instance=serializer.save()
+        instance.set_password(instance.password)
+        instance.save()
+
