52ae6c3f9462d7cd8c00bbc405c11e901224a209
muhwezi deo <aggrey256@gmail.com>
2018-12-06 15:07:06 +0300

    create login view

--- fast_food_api/users/urls.py
+++ fast_food_api/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('signup/',views.UserView.as_view(),name='signup')
+    path('signup/',views.UserView.as_view(),name='signup'),
+    path('login/',views.LoginView.as_view(),name='login'),
 ]
--- fast_food_api/users/views.py
+++ fast_food_api/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
 from . import serializers
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 # Create your views here.
 
 class UserView(generics.CreateAPIView):
@@ -11,3 +15,17 @@ class UserView(generics.CreateAPIView):
         instance.set_password(instance.password)
         instance.save()
 
+class LoginView(APIView):
+    authentication_classes=()
+    def post(self,request,):
+        username=request.data.get("username")
+        password=request.data.get("password")
+        user =authenticate(username=username,password=password)
+        if user:
+            return Response({
+                "token":user.auth_token.key
+            })
+        return Response({
+            "message":"We cant authenticate u {}".format(username)
+        })
+        
