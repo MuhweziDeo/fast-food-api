587501c9b73a4c1e7aa3448480253f23770b5c60
muhwezi deo <aggrey256@gmail.com>
2018-12-06 15:45:58 +0300

    add logic to login user

--- fast_food_api/users/views.py
+++ fast_food_api/users/views.py
@@ -5,6 +5,7 @@ from . import serializers
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
+from . import models
 # Create your views here.
 
 class UserView(generics.CreateAPIView):
@@ -20,8 +21,10 @@ class LoginView(APIView):
     def post(self,request,):
         username=request.data.get("username")
         password=request.data.get("password")
-        user =authenticate(username=username,password=password)
-        if user:
+        user =models.User.objects.get(email=username)
+        print(user.password)
+        if user.password==password:
+            Token.objects.get_or_create(user=user)
             return Response({
                 "token":user.auth_token.key
             })
