c2959106b7f8f56c8e4e6d1dcae982788fbef698
muhwezi deo <aggrey256@gmail.com>
2018-12-06 09:07:08 +0300

    fix password hashing

--- fast_food_api/users/serializers.py
+++ fast_food_api/users/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework.authtoken.models import Token
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from . import models
-from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=models.User
         fields=('id','username','email','password')
         extra_kwargs={'password':{'write_only':True}}
-        def create(self,validated_data):
-            user=models.User.objects.create_user(
+    def create(self,validated_data):
+        user=models.User(
                 username=validated_data['username'],
                 email=validated_data['email']
-            )
-            user.set_password(validated_data['password'])
-            user.save()
-            return user
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
